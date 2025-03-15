47672fd2ec99ba2fd6a3068ed80b2bba76ee2348
zhaoya <12462303+jungen2023@user.noreply.gitee.com>
2025-03-15 05:37:54 +0000

    update shell/difyapi-dns.sh.
    Signed-off-by: zhaoya <12462303+jungen2023@user.noreply.gitee.com>

--- shell/difyapi-dns.sh
+++ shell/difyapi-dns.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 ip=(`docker inspect docker-api-1 |awk -F: '/IPAddress/{print $2}'|tail -n2|awk -F'"' '{print $2}'`)
 if [[ -n "${ip[@]}" ]];then
-   web_ip=${ip[0]}
-   api_ip=${ip[1]}
+   api_ip=${ip[0]}
+   web_ip=${ip[1]}
 else
    exit 0
-fi
-
-web=`grep web /etc/hosts`
-if [ -z "${web}" ];then
-   echo "${web_ip} web" >> /etc/hosts
-else
-   webd_ip=`awk '/web/{print $1}' /etc/hosts`
-   if [[ "${webd_ip}" != "${web_ip}" ]];then
-      sed -i "/web/s/${webd_ip}/${web_ip}/g" /etc/hosts
-   fi
 fi
 
 api=`grep api /etc/hosts`
@@ -24,5 +14,15 @@ else
    apid_ip=`awk '/api/{print $1}' /etc/hosts`
    if [[ "${apid_ip}" != "${api_ip}" ]];then
       sed -i "/api/s/${apid_ip}/${api_ip}/g" /etc/hosts
+   fi
+fi
+
+web=`grep web /etc/hosts`
+if [ -z "${web}" ];then
+   echo "${web_ip} web" >> /etc/hosts
+else
+   webd_ip=`awk '/web/{print $1}' /etc/hosts`
+   if [[ "${webd_ip}" != "${web_ip}" ]];then
+      sed -i "/web/s/${webd_ip}/${web_ip}/g" /etc/hosts
    fi
 fi
